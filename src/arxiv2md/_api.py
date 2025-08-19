788e9ee004a731129b662222c74f7eeb296ba3d9
komiya <90894519+misya11p@users.noreply.github.com>
2025-08-19 23:12:24 +0900

    arxiv2md関数をCLIから呼び出すように変更し、コードを整理

--- src/arxiv2md/_api.py
+++ src/arxiv2md/_api.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import tempfile
 
+from halo import Halo
+
 from ._utils import extract_arxiv_id
 from ._get_source import get_source
 from ._convert import tex2xml, JATSConverter
@@ -9,16 +11,59 @@ from ._convert import tex2xml, JATSConverter
 DNAME_SOURCE_ARXIV = "arxiv_source"
 
 
-def arxiv2md(url: str) -> str:
-    arxiv_id = extract_arxiv_id(url)
+def _core_arxiv2md(arxiv_id, dpath_source, enable_spinner) -> str:
+    dpath_source = Path(dpath_source).resolve()
+    if not dpath_source.exists():
+        dpath_source.mkdir(parents=True)
+    dpath_source_arxiv = dpath_source / DNAME_SOURCE_ARXIV
 
-    with tempfile.TemporaryDirectory() as tmpdir:
-        dpath_temp = Path(tmpdir)
-        dpath_source = dpath_temp / DNAME_SOURCE_ARXIV
+    if enable_spinner: # When used in CLI, spinner is enabled
+        with Halo(
+            text=f"Get source for arXiv:{arxiv_id}",
+            spinner="dots",
+        ) as spinner:
+            get_source(arxiv_id, dpath_source_arxiv)
+            spinner.succeed()
 
-        get_source(arxiv_id, dpath_source)
-        fpath_jats = tex2xml(dpath_source)
+        with Halo(
+            text=f"Convert to Markdown",
+            spinner="dots",
+        ) as spinner:
+            fpath_jats = tex2xml(dpath_source_arxiv)
+            converter = JATSConverter(fpath_jats)
+            content_md = converter.convert_to_md()
+            spinner.succeed()
+
+    else:
+        # When used as a library, spinner is not used.
+        # `Halo` doesn't work properly in ipython, so the `enabled`
+        # option won't be used.
+        get_source(arxiv_id, dpath_source_arxiv)
+        fpath_jats = tex2xml(dpath_source_arxiv)
         converter = JATSConverter(fpath_jats)
         content_md = converter.convert_to_md()
+
+    return content_md
+
+
+def arxiv2md(url: str, dpath_source: str | None = None) -> str:
+    arxiv_id = extract_arxiv_id(url)
+
+    if dpath_source:
+        content_md = _core_arxiv2md(arxiv_id, dpath_source, False)
+    else:
+        with tempfile.TemporaryDirectory() as tempdir:
+            content_md = _core_arxiv2md(arxiv_id, tempdir, False)
+
+    return content_md
+
+def arxiv2md_cli(url: str, dpath_source: str | None = None) -> str:
+    arxiv_id = extract_arxiv_id(url)
+
+    if dpath_source:
+        content_md = _core_arxiv2md(arxiv_id, dpath_source, True)
+    else:
+        with tempfile.TemporaryDirectory() as tempdir:
+            content_md = _core_arxiv2md(arxiv_id, tempdir, True)
 
     return content_md
--- src/arxiv2md/cli.py
+++ src/arxiv2md/cli.py
@@ -1,13 +1,9 @@
-import tempfile
 from pathlib import Path
 
 import typer
-from halo import Halo
 
-from ._api import DNAME_SOURCE_ARXIV
 from ._utils import extract_arxiv_id
-from ._get_source import get_source
-from ._convert import tex2xml, JATSConverter
+from ._api import arxiv2md_cli
 
 
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
@@ -59,15 +55,7 @@ def cli(
             ):
                 raise typer.Exit()
 
-    if dpath_source:
-        content_md = convert_arxiv_to_md(
-            dpath_source, arxiv_id, enabled_spinner=not stdout
-        )
-    else:
-        with tempfile.TemporaryDirectory() as tempdir:
-            content_md = convert_arxiv_to_md(
-                tempdir, arxiv_id, enabled_spinner=not stdout
-            )
+    content_md = arxiv2md_cli(arxiv_id, dpath_source)
 
     if stdout:
         print(content_md)
@@ -75,30 +63,3 @@ def cli(
         with open(fpath_output, "w", encoding="utf-8") as f:
             f.write(content_md)
         print(f"Markdown file saved to `{fpath_output}`")
-
-
-def convert_arxiv_to_md(dpath_source, arxiv_id, enabled_spinner):
-    dpath_source = Path(dpath_source).resolve()
-    if not dpath_source.exists():
-        dpath_source.mkdir(parents=True)
-    dpath_source_arxiv = dpath_source / DNAME_SOURCE_ARXIV
-
-    with Halo(
-        text=f"Get source for arXiv:{arxiv_id}",
-        spinner="dots",
-        enabled=enabled_spinner,
-    ) as spinner:
-        get_source(arxiv_id, dpath_source_arxiv)
-        spinner.succeed()
-
-    with Halo(
-        text=f"Convert to Markdown",
-        spinner="dots",
-        enabled=enabled_spinner,
-    ) as spinner:
-        fpath_jats = tex2xml(dpath_source_arxiv)
-        converter = JATSConverter(fpath_jats)
-        content_md = converter.convert_to_md()
-        spinner.succeed()
-
-    return content_md
