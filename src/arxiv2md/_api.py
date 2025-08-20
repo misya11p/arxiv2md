71ad57014762139bc9b356427fc27fb19164ce20
komiya <90894519+misya11p@users.noreply.github.com>
2025-08-20 17:12:01 +0900

    coreを分けた

--- src/arxiv2md/_api.py
+++ src/arxiv2md/_api.py
@@ -1,8 +1,6 @@
 from pathlib import Path
 import tempfile
 
-from halo import Halo
-
 from ._utils import extract_arxiv_id
 from ._get_source import get_source
 from ._convert import tex2xml, JATSConverter
@@ -11,37 +9,55 @@ from ._convert import tex2xml, JATSConverter
 DNAME_SOURCE_ARXIV = "arxiv_source"
 
 
-def _core_arxiv2md(arxiv_id, dpath_source, enable_spinner) -> str:
+def _core_arxiv2md_cli(arxiv_id, dpath_source) -> str:
+    from halo import Halo
+
     dpath_source = Path(dpath_source).resolve()
     if not dpath_source.exists():
         dpath_source.mkdir(parents=True)
     dpath_source_arxiv = dpath_source / DNAME_SOURCE_ARXIV
 
-    if enable_spinner: # When used in CLI, spinner is enabled
-        with Halo(
-            text=f"Get source for arXiv:{arxiv_id}",
-            spinner="dots",
-        ) as spinner:
-            get_source(arxiv_id, dpath_source_arxiv)
-            spinner.succeed()
-
-        with Halo(
-            text=f"Convert to Markdown",
-            spinner="dots",
-        ) as spinner:
-            fpath_jats = tex2xml(dpath_source_arxiv)
-            converter = JATSConverter(fpath_jats)
-            content_md = converter.convert_to_md()
-            spinner.succeed()
-
-    else:
-        # When used as a library, spinner is not used.
-        # `Halo` doesn't work properly in ipython, so the `enabled`
-        # option won't be used.
+    with Halo(
+        text=f"Get source for arXiv:{arxiv_id}",
+        spinner="dots",
+    ) as spinner:
         get_source(arxiv_id, dpath_source_arxiv)
+        spinner.succeed()
+
+    with Halo(
+        text=f"Convert to Markdown",
+        spinner="dots",
+    ) as spinner:
         fpath_jats = tex2xml(dpath_source_arxiv)
         converter = JATSConverter(fpath_jats)
         content_md = converter.convert_to_md()
+        spinner.succeed()
+
+    return content_md
+
+
+def _core_arxiv2md(arxiv_id, dpath_source) -> str:
+    dpath_source = Path(dpath_source).resolve()
+    if not dpath_source.exists():
+        dpath_source.mkdir(parents=True)
+    dpath_source_arxiv = dpath_source / DNAME_SOURCE_ARXIV
+
+    get_source(arxiv_id, dpath_source_arxiv)
+    fpath_jats = tex2xml(dpath_source_arxiv)
+    converter = JATSConverter(fpath_jats)
+    content_md = converter.convert_to_md()
+
+    return content_md
+
+
+def arxiv2md_cli(url: str, dpath_source: str | None = None) -> str:
+    arxiv_id = extract_arxiv_id(url)
+
+    if dpath_source:
+        content_md = _core_arxiv2md_cli(arxiv_id, dpath_source)
+    else:
+        with tempfile.TemporaryDirectory() as tempdir:
+            content_md = _core_arxiv2md_cli(arxiv_id, tempdir)
 
     return content_md
 
@@ -50,20 +66,9 @@ def arxiv2md(url: str, dpath_source: str | None = None) -> str:
     arxiv_id = extract_arxiv_id(url)
 
     if dpath_source:
-        content_md = _core_arxiv2md(arxiv_id, dpath_source, False)
+        content_md = _core_arxiv2md(arxiv_id, dpath_source)
     else:
         with tempfile.TemporaryDirectory() as tempdir:
-            content_md = _core_arxiv2md(arxiv_id, tempdir, False)
-
-    return content_md
-
-def arxiv2md_cli(url: str, dpath_source: str | None = None) -> str:
-    arxiv_id = extract_arxiv_id(url)
-
-    if dpath_source:
-        content_md = _core_arxiv2md(arxiv_id, dpath_source, True)
-    else:
-        with tempfile.TemporaryDirectory() as tempdir:
-            content_md = _core_arxiv2md(arxiv_id, tempdir, True)
+            content_md = _core_arxiv2md(arxiv_id, tempdir)
 
     return content_md
