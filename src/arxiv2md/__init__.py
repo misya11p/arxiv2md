dcdb8e23babd1be82eb24a91d21d041826e2fcf8
komiya <90894519+misya11p@users.noreply.github.com>
2025-08-27 23:38:37 +0900

    import文の整理とPackageNotFoundErrorのハンドリングを追加

--- src/arxiv2md/__init__.py
+++ src/arxiv2md/__init__.py
@@ -1,4 +1,10 @@
 from ._api import arxiv2md
 
-from importlib.metadata import version as _version
-__version__ = _version(__package__)
+from importlib.metadata import (
+    version as _version,
+    PackageNotFoundError
+)
+try:
+    __version__ = _version(__package__)
+except PackageNotFoundError:
+    pass
